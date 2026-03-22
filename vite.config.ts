81130c23c6545de350bbdde701311517244a48ef
Jesper Lindberg <Jillypuff@protonmail.com>
2026-03-22 01:19:40 +0100

    fixed for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    tanstackStart(),
-    devtools(),
     tsconfigPaths(),
     tailwindcss(),
+    tanstackStart(),
   ],
 })
