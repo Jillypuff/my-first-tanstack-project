6957575006484b8b9e3bd0f603b8712d8a24a5f5
Jesper Lindberg <Jillypuff@protonmail.com>
2026-03-22 01:00:40 +0100

    comon vercel work with me

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     tanstackStart(),
     devtools(),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tsconfigPaths(),
     tailwindcss(),
   ],
 })
